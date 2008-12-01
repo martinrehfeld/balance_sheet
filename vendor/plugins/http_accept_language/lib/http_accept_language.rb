f670bad12a92dd81e9962147b9793107c22dcfcd
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2008-12-01 10:06:07 +0100

    Add mirror 'vendor/plugins/http_accept_language/'.

---
+++ vendor/plugins/http_accept_language/lib/http_accept_language.rb
@@ -0,0 +1,20 @@
+module HttpAcceptLanguage
+  # Returns a sorted array based on user preference in HTTP_ACCEPT_LANGUAGE.
+  # Browsers send this HTTP header. Don't think this is holy.
+  def user_preferred_languages
+    @user_preferred_languages ||= env['HTTP_ACCEPT_LANGUAGE'].split(',').collect do |l|
+      l += ';q=1.0' unless l =~ /;q=\d+\.\d+$/
+      l.split(';q=')
+    end.sort do |x,y|
+      raise "Not correctly formatted" unless x.first =~ /^[a-z\-]+$/i
+      y.last.to_f <=> x.last.to_f
+    end.collect do |l|
+      l.first.downcase.gsub(/-[a-z]+$/i){|x| x.upcase} 
+    end
+  rescue
+    []
+  end
+  def preferred_language_from(array)
+    (user_preferred_languages & array).first
+  end
+end
