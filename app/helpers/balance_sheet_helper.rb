7944e8f02e98364e8ece2c7ffe6ce297aaa6531f
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2008-12-01 13:28:11 +0100

    refactored chart helper

--- app/helpers/balance_sheet_helper.rb
+++ app/helpers/balance_sheet_helper.rb
@@ -1,36 +1,28 @@
 module BalanceSheetHelper
   
   def google_chart_url(dataset, colors)
-    data_values = []
-    set_colors = []
-    set_labels = []
+    data_values, set_colors, set_labels = [], [], []
     
-    # collect credit data
-    colors.reverse_each do |colspec|
-      risk_class_id = colspec.first
-      values = dataset.collect {|m| m[:credit_by_risk_class][risk_class_id].to_i / 1000.0 }
-      if !values.all?(&:zero?)
-        set_colors << colspec[1][:credit]
-        set_labels << "#{t 'balance_sheet.credit'}: #{colspec[1][:label]}"
-        data_values << values
+    # collect credit/debit data, colors and labels for legend
+    [ {:reverse_each => :credit}, {:each => :debit} ].each do |processing_instruction|
+      iterator, key = processing_instruction.keys.first, processing_instruction.values.first
+      colors.send iterator do |colspec|
+        risk_class_id = colspec.first
+        values = dataset.collect {|m| m[:"#{key}_by_risk_class"][risk_class_id].to_i / 1000.0 }
+        if !values.all?(&:zero?)
+          set_colors << colspec[1][key]
+          set_labels << "#{t "balance_sheet.#{key}"}: #{colspec[1][:label]}"
+          data_values << values
+        end
       end
     end
-
-    # collect debit data 
-    colors.each do |colspec|
-      risk_class_id = colspec.first
-      values = dataset.collect {|m| m[:debit_by_risk_class][risk_class_id].to_i / 1000.0 }
-      if !values.all?(&:zero?)
-        set_colors << colspec[1][:debit]
-        set_labels << "#{t 'balance_sheet.debit'}: #{colspec[1][:label]}"
-        data_values << values
-      end
-    end
-    
+        
+    # collect x axis labels
     total_labels = dataset.collect {|m| number_with_delimiter(m[:total].to_i) }
     month_labels = dataset.collect {|m| m[:date].to_datetime.to_s(:short_month) }
     year_labels = dataset.collect {|m| m[:date].month == 1 ? m[:date].year : "" }
     
+    # calculate y axis bounds
     lower_bound = dataset.collect {|m| m[:debit] }.min.to_i / 1000.0
     upper_bound = dataset.collect {|m| m[:credit] }.max.to_i / 1000.0
     margin = [lower_bound, upper_bound].map(&:abs).max * 0.1 # 10% margin to x-axis
