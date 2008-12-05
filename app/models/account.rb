eccde681674154560dee5b07c11ba238915b819b
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2008-12-06 00:42:47 +0100

    cleaned up Account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -6,15 +6,14 @@ class Account < ActiveRecord::Base
   has_many   :entries
   
   def total(date = Date.civil(9999,12,31))
-    total = balance(date, true)
-    total = -total if self.liability?
+    total = inverted_balance(date, true)
 
     # add non-cumulative entries' values to balance
     entries_in_reverse_chronological_order(date).reject {|e| e.entry_type.cumulative? }.each do |e|
       total += e.value if e.value
     end
 
-    self.liability? ? -total : total
+    liability_corrected_value total
   end
   
   def balance(date = Date.civil(9999,12,31), include_future_payment_balance = false)
@@ -23,43 +22,38 @@ class Account < ActiveRecord::Base
     # find latest cumulative entry
     balance = entries_in_reverse_chronological_order(date).detect(Proc.new { Entry.new(:value => 0) }) {|e| e.entry_type.cumulative? }.value
 
-    self.liability? ? -balance : balance
+    liability_corrected_value balance
+  end
+  
+  def inverted_balance(date = Date.civil(9999,12,31), include_future_payment_balance = false)
+    total = balance(date, include_future_payment_balance)
+    liability_corrected_value total
   end
   
   def payouts_till(date)
-    if self.show_balance_as_future_payment?
-      payouts = balance(date, true)
-      payouts = -payouts if self.liability?
-    else
-      payouts = 0.0
-    end
+    payouts = self.show_balance_as_future_payment ? inverted_balance(date, true) : 0.0
     payouts = 0.0 if payouts > 0
       
-    payouts += (entries_in_reverse_chronological_order(date).reject {|e| e.entry_type.cumulative? || e.value.nil? }.map {|e|
+    payouts += non_cumulative_entry_sum(date) {|e|
       if self.liability?
         e.value > 0 ? e.value : 0.0
       else
         e.value < 0 ? -e.value : 0.0
       end
-    }.sum || 0.0)
+    }
   end
   
   def deposits_till(date)
-    if self.show_balance_as_future_payment?
-      deposits = balance(date, true)
-      deposits = -deposits if self.liability?
-    else
-      deposits = 0.0
-    end
+    deposits = self.show_balance_as_future_payment ? inverted_balance(date, true) : 0.0
     deposits = 0.0 if deposits < 0
   
-    deposits += (entries_in_reverse_chronological_order(date).reject {|e| e.entry_type.cumulative? || e.value.nil? }.map {|e|
+    deposits += non_cumulative_entry_sum(date) {|e|
       if self.liability?
         e.value > 0 ? 0.0 : -e.value
       else
         e.value < 0 ? 0.0 : e.value
       end
-    }.sum || 0.0)
+    }
   end
 
   class << self
@@ -77,10 +71,23 @@ class Account < ActiveRecord::Base
     end
   end
 
+private
+
   def entries_in_reverse_chronological_order(last_date)
     self.entries.find(:all, :joins => :entry_type,
                       :conditions => ['entries.effective_date <= ?', last_date],
                       :order => 'effective_date DESC')
   end
   memoize :entries_in_reverse_chronological_order
+  
+  def non_cumulative_entry_sum(date, &block)
+    block ||= Proc.new {|e| e.value}
+    entries_in_reverse_chronological_order(date).reject {|e|
+      e.entry_type.cumulative? || e.value.nil?
+    }.map(&block).sum || 0.0
+  end
+  
+  def liability_corrected_value(value) 
+    self.liability? ? -value : value
+  end
 end
