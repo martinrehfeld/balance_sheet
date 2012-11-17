caca24fb5b5b7bd3e0a0af23f736236b02c25e96
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2012-11-17 16:46:29 +0100

    add route for asset growth chart

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :risk_classes
   map.resources :accounts
   map.root      :controller => 'balance_sheet'
+  map.connect   'asset_growth_chart.:format', :controller => 'balance_sheet'
   map.connect   'balances_by_risk_class_chart.:format', :controller => 'balance_sheet'
   map.connect   'future_payments_chart.:format', :controller => 'balance_sheet', :action => 'future_payments'
   map.connect   'balances_by_account_class_chart.:format', :controller => 'balance_sheet', :action => 'account_classes'
